401bc1fd6e58a77311e7d5c0a8acd4090a19e783
Angel <123137.com@gmail.com>
2023-04-25 12:17:57 -0700

    removeTab

--- tests/deleteKit-Response.test.js
+++ tests/deleteKit-Response.test.js
@@ -2,17 +2,17 @@
 const config = require('../config');
 
 test('ok field should be true', async () => {
-    let response;
-    try {
-		 response = await fetch(`${config.API_URL}/api/v1/kits/2`, {
-			method: 'DELETE',
+	let response;
+	try {
+	 response = await fetch(`${config.API_URL}/api/v1/kits/2`, {
+	 method: 'DELETE',
 		});
-       
+	   
 	} catch (error) {
 		console.error(error);
 	}   
-    const data = await response.json();
-    console.log(data);
+	const data = await response.json();
+	console.log(data);
 // the response data should be TRUE if the kit exists in the system 
 expect(data.ok).toBe(true);
 
