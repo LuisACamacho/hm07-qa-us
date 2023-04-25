72c88ee86ada48ac983a256034406b220382baf0
Angel <123137.com@gmail.com>
2023-04-25 12:20:35 -0700

    deletespaces

--- tests/deleteKit-Response.test.js
+++ tests/deleteKit-Response.test.js
@@ -4,10 +4,9 @@ const config = require('../config');
 test('ok field should be true', async () => {
 	let response;
 	try {
-	 response = await fetch(`${config.API_URL}/api/v1/kits/2`, {
-	 method: 'DELETE',
-		});
-	   
+		 response = await fetch(`${config.API_URL}/api/v1/kits/2`, {
+		 method: 'DELETE',
+		});   
 	} catch (error) {
 		console.error(error);
 	}   
