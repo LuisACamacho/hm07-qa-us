0ee3aaaefd68d50c1ae3c3ebc1e7909e1ba50ee4
Angel <123137.com@gmail.com>
2023-04-25 12:08:28 -0700

    new url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://48305e89-1613-42bc-b948-c21a02582bdc.serverhub.practicum-services.com"
+    API_URL: "https://d6181b29-0c89-457a-af9b-f2dc110eb03b.serverhub.practicum-services.com"
 }  
